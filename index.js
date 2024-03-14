13f50be0ef3abcdfba8826ca1ec10c1490cac28d
Nilavazhagan Elangovan <nilavazhagan.elangovan@goodai.com>
2024-03-14 19:15:33 +0530

    Added image carousel

--- index.js
+++ index.js
@@ -36,3 +36,50 @@ const headerLogoConatiner = document.querySelector('.header__logo-container')
 headerLogoConatiner.addEventListener('click', () => {
   location.href = 'index.html'
 })
+
+function InitCarousel(carouselContainer)
+{
+  const delay = 3000; //ms
+
+  const slides = carouselContainer.querySelector(".slides");
+  const slidesCount = slides.childElementCount;
+  const maxLeft = (slidesCount - 1) * 100 * -1;
+
+  let current = 0;
+
+  function changeSlide(next = true) {
+    if (next) {
+      current += current > maxLeft ? -100 : current * -1;
+    } else {
+      current = current < 0 ? current + 100 : maxLeft;
+    }
+
+    slides.style.left = current + "%";
+  }
+
+  let autoChange = setInterval(changeSlide, delay);
+  const restart = function() {
+    clearInterval(autoChange);
+    autoChange = setInterval(changeSlide, delay);
+  };
+
+// Controls
+  carouselContainer.querySelector(".next-slide").addEventListener("click", function() {
+    changeSlide();
+    restart();
+  });
+
+  carouselContainer.querySelector(".prev-slide").addEventListener("click", function() {
+    changeSlide(false);
+    restart();
+  });
+}
+
+const carousels = document.querySelectorAll(".carousel");
+for (let i = 0, count = carousels.length; i < count; i++ )
+{
+  InitCarousel(carousels[i]);
+}
+
+
+
