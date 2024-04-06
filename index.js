dbb22bc63f2bc1b73b8aec3aea14a57cfdd4bb22
Nilavazhagan <nilavazhagan1696@gmail.com>
2024-04-06 20:25:36 +0530

    Added project "The Call" to projects section and moved Pop-It and DeadEnd to More Projects section

--- index.js
+++ index.js
@@ -2,10 +2,25 @@
 const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
 const smallMenu = document.querySelector('.header__sm-menu')
 const headerHamMenuBtn = document.querySelector('.header__main-ham-menu')
+const moreProjectsBtn = document.querySelector('#more-projects-button')
+const moreProjectsHeading = document.querySelector('.heading.heading-sec.heading-sec__mb-bg.more');
 const headerHamMenuCloseBtn = document.querySelector(
   '.header__main-ham-menu-close'
 )
 const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link')
+
+moreProjectsHeading.addEventListener("click", () => {
+  var hidden_section = document.querySelector('.projects__content.more');
+  moreProjectsBtn.classList.toggle("up");
+  moreProjectsBtn.classList.toggle("down");
+  hidden_section.classList.toggle("shown");
+  hidden_section.classList.toggle("hidden");
+  if (hidden_section.classList.contains('shown')) {
+    hidden_section.style.maxHeight = hidden_section.scrollHeight + 'px';
+  } else {
+    hidden_section.style.maxHeight = '0';
+  }
+})
 
 hamMenuBtn.addEventListener('click', () => {
   if (smallMenu.classList.contains('header__sm-menu--active')) {
